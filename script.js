e49441fd0e66db4ce9f97ffb6d6bef1af092551b
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-04-08 03:46:55 -0300

    As cartas mudam a imagem quando clicadas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Testando o numero de cartas
 
-let numerocartas = prompt("qual o numero de cartas?");
+// let numerocartas = prompt("qual o numero de cartas?");
+let numerocartas = 6
 
 let testecartas = (numerocartas < 4) || (numerocartas > 14) || (numerocartas%2==1);
 
@@ -12,7 +13,6 @@ while(testecartas){
 // Criando array com as imagens
 
 const imagens = ['Imagens/bobrossparrot.gif','Imagens/explodyparrot.gif','Imagens/fiestaparrot.gif','Imagens/metalparrot.gif','Imagens/revertitparrot.gif','Imagens/tripletsparrot.gif','Imagens/unicornparrot.gif']
-
 // Adicionando as cartas com imagens unicas duas a duas
 
 let qtdecartas = document.querySelector(".cartas");
@@ -20,18 +20,28 @@ let carta = document.querySelector(".cartas .carta img");
 
 for(let i = 0; i < numerocartas; i++){
     if(i<2){
-    qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[0]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[0]} class="gifs esconder"></div>`;
     }else if(i<4){
-        qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[1]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[1]} class="gifs esconder"></div>`;
     }else if(i<6){
-        qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[2]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[2]} class="gifs esconder"></div>`;
     }else if(i<8){
-        qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[3]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[3]} class="gifs esconder"></div>`;
     }else if(i<10){
-        qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[4]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[4]} class="gifs esconder"></div>`;
     }else if(i<12){
-        qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[5]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[5]} class="gifs esconder"></div>`;
     }else if(i<14){
-        qtdecartas.innerHTML +=`<div class="carta"><img src="Imagens/front.png"><img src=${imagens[6]} class="gifs"></div>`;
+        qtdecartas.innerHTML +=`<div class="carta" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[6]} class="gifs esconder"></div>`;
     }
+}
+
+function virarcarta(elementoClicado){
+    console.log(elementoClicado);
+    const papagaio = elementoClicado.querySelector(".costa-carta");
+    const gif = elementoClicado.querySelector(".gifs");
+
+    papagaio.classList.toggle("esconder");
+    gif.classList.toggle("esconder");
+
 }
