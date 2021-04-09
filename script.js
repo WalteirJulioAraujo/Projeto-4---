382cd2d202bf4f9a43788fcf389263634d880a04
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-04-09 02:54:04 -0300

    limpeza no js, total de jogadas,e  rotação

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Testando o numero de cartas
 
 // let numerocartas = prompt("qual o numero de cartas?");
-let numerocartas = 6
+let numerocartas = 4;
 
 let testecartas = (numerocartas < 4) || (numerocartas > 14) || (numerocartas%2==1);
 
@@ -13,7 +13,7 @@ while(testecartas){
 // Criando array com as imagens
 
 const imagens = ['Imagens/bobrossparrot.gif','Imagens/explodyparrot.gif','Imagens/fiestaparrot.gif','Imagens/metalparrot.gif','Imagens/revertitparrot.gif','Imagens/tripletsparrot.gif','Imagens/unicornparrot.gif'];
-// const todascartas2 = [`<div class="carta carta1" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[0]} class="gifs esconder"></div>`,`<div class="carta carta2" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[1]} class="gifs esconder"></div>`,`<div class="carta carta3" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[2]} class="gifs esconder"></div>`,`<div class="carta carta4" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[3]} class="gifs esconder"></div>`,`<div class="carta carta5" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[4]} class="gifs esconder"></div>`,`<div class="carta carta6" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[5]} class="gifs esconder"></div>`,`<div class="carta carta7" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[6]} class="gifs esconder"></div>`];
+imagens.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
 let metadecartas1=[];
 let metadecartas2=[];
@@ -22,78 +22,63 @@ for(let i = 0; i < (numerocartas/2); i++){
     metadecartas2.push(`<div class="carta carta${i}" onclick="virarcarta(this)"><img src="Imagens/front.png" class="costa-carta"><img src=${imagens[i]} class="gifs esconder"></div>`);
 }
 
+let totaldecartas = metadecartas1.concat(metadecartas2);
+
 // Embaralhando as arrays
-imagens.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-metadecartas1.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-metadecartas2.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+totaldecartas.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
 let qtdecartas = document.querySelector(".cartas");
-for(let i = 0; i < numerocartas/2; i++){
-    qtdecartas.innerHTML += `${metadecartas1[i]}`;
-    qtdecartas.innerHTML += `${metadecartas2[i]}`;
-}
-
-
-/* Fiz tentando add duas a duas -Adicionando as cartas com imagens unicas duas a duas
 for(let i = 0; i < numerocartas; i++){
-    if(i<2){
-        qtdecartas.innerHTML +=`${todascartas[0]}`;
-    }else if(i<4){
-        qtdecartas.innerHTML +=`${todascartas[1]}`;
-    }else if(i<6){
-        qtdecartas.innerHTML +=`${todascartas[2]}`;
-    }else if(i<8){
-        qtdecartas.innerHTML +=`${todascartas[3]}`;
-    }else if(i<10){
-        qtdecartas.innerHTML +=`${todascartas[4]}`;
-    }else if(i<12){
-        qtdecartas.innerHTML +=`${todascartas[5]}`;
-    }else if(i<14){
-        qtdecartas.innerHTML +=`${todascartas[6]}`;
-    }
+    qtdecartas.innerHTML += `${totaldecartas[i]}`;
 }
-*/
 
 let ultimacarta = null;
 let contadorpares = 0; 
 const paresvitoria = numerocartas / 2 ;
+let jogadas = 0;
 
 function virarcarta(elementoClicado){
-    console.log(elementoClicado);
-    const papagaio = elementoClicado.querySelector(".costa-carta");
-    const gif = elementoClicado.querySelector(".gifs");
-
-    console.log(elementoClicado.classList.value);
+    elementoClicado.classList.add("rotacionar");
     if(ultimacarta === null){
         ultimacarta = elementoClicado;
         ultimacarta.querySelector(".costa-carta").classList.add("esconder");
         ultimacarta.querySelector(".gifs").classList.remove("esconder");
+        jogadas++;
 
     }else{
+
+        jogadas++;
+
         elementoClicado.querySelector(".costa-carta").classList.add("esconder");
         elementoClicado.querySelector(".gifs").classList.remove("esconder");
 
         if(ultimacarta.classList.value === elementoClicado.classList.value){
             console.log("igual");
             contadorpares++;
+            
         } else {
-            ultimacarta.querySelector(".costa-carta").classList.remove("esconder");
-            ultimacarta.querySelector(".gifs").classList.add("esconder");
-            elementoClicado.querySelector(".costa-carta").classList.remove("esconder");
-            elementoClicado.querySelector(".gifs").classList.add("esconder");
+            setTimeout(Desvira,1000,elementoClicado,ultimacarta);
         }
         ultimacarta = null;
     }
 
-    vencer();
+    setTimeout(vencer,100);
 }
 
 function vencer(){
     if(contadorpares === paresvitoria){
-        alert("Voce venceu");
+        alert(`Você venceu com ${jogadas} jogadas!`);
     }
 }
 
+function Desvira(elementoClicado,ultimacarta){
+    ultimacarta.querySelector(".costa-carta").classList.remove("esconder");
+    ultimacarta.querySelector(".gifs").classList.add("esconder");
+    elementoClicado.querySelector(".costa-carta").classList.remove("esconder");
+    elementoClicado.querySelector(".gifs").classList.add("esconder");
+    elementoClicado.classList.remove("rotacionar");
+    ultimacarta.classList.remove("rotacionar");
+}
 
 // Funcao que embaralha
 function comparador() { 
